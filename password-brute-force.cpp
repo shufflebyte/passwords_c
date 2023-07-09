151d41b26e451fe376d68436d2e66b2682741bac
Florian Soulier <florian@Florians-MBP-2.fritz.box>
2023-07-09 14:58:38 +0200

    vor dem versuch, die alphabete anzupassen

--- password-brute-force.cpp
+++ password-brute-force.cpp
@@ -5,8 +5,13 @@
 // MacOS: brew install openssl
 // Windows: https://sourceforge.net/projects/libssl/files/latest/download herunterladen und in den Ordner zur cpp Datei legen
 #include <openssl/sha.h>
+#include <stdlib.h>
 
 using namespace std;
+
+clock_t beginClock;
+clock_t endClock;
+bool foundPassword = false;
 
 enum Kind
 {
@@ -35,88 +40,114 @@ string sha256(const string str)
     return ss.str();
 }
 
-string bruteForce(string hash, int length, int startASCII, int endASCII)
+static const char alphabet_num[] = "0123456789";
+
+static const char alphabet_alpha[] = "abcdefghijklmnopqrstuvwxyz";
+
+//static const char alphabet_alphanumspecial[] =
+static const char alphabet[] =
+"abcdefghijklmnopqrstuvwxyz"
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"0123456789";
+
+/*static const char alphabet[] =
+    "abcdefg";
+    */
+
+int alphabetSize = sizeof(alphabet) - 1;
+
+void bruteImpl(char *str, int index, int maxDepth, string password)//, char alphabet[])
 {
-    char password[length + 1];
-    for (int i = 0; i <= length; i++)
+    for (int i = 0; i < alphabetSize && foundPassword == false; ++i)
     {
-        password[i] = '\0';
-    }
+        str[index] = alphabet[i];
 
-    bool foundPassword=false;
-
-    for (int cntStellen = 0; (cntStellen < length && foundPassword==false); cntStellen++)
-    {
-        // immer wenn Länge um 1 erhöht wird
-        // password[cntStellen - 1] = startASCII;
-        for (int i = 0; i <= cntStellen; i++)
+        if (index == maxDepth - 1)
         {
-            password[i] = 'a';
-        }
+            // printf("%s\n", str);
 
-        int merkeDurchlaeufe = 0;
-        // optimization für ersten Durchlauf
-        if (cntStellen == 0)
-        {
-            merkeDurchlaeufe = endASCII - startASCII;
-        }
-
-        // wiederhole für jede Stelle endASCII-startASCII+1 mal
-        for (; (merkeDurchlaeufe < endASCII - startASCII + 1) && foundPassword ==false; merkeDurchlaeufe++)
-        {
-
-            for (int cntChar = startASCII; cntChar < endASCII + 1; cntChar++)
+            if (sha256(str) == sha256(password))
             {
-                password[cntStellen] = cntChar;
-                cout << password << endl;
-                if (sha256(password) == sha256("zzzzzzzzz"))
-                {
-                    foundPassword=true;
-                    cout << "FOUND!!!!!!!\n";
-                    cout << password << sha256(password) << sha256("zzzzzzzzz");
-                    break;
-                }
-            }
-            if (cntStellen > 0 && password[cntStellen - 1] < endASCII && foundPassword==false)
-            {
-                password[cntStellen - 1] = password[cntStellen - 1] + 1;
+                cout << endl
+                     << "YESS" << str << sha256(password);
+                endClock = clock();
+                cout << "bruteimpl" << clock() << endl;
+                foundPassword = true;
             }
         }
+        else
+            bruteImpl(str, index + 1, maxDepth, password);//, alphabet);
     }
-    cout << "WARUM?" << password;
-    return password;
 }
 
-int main()
+void bruteSequential(int maxLen, string password)//, char alphabet[])
 {
-    // Einlesen
-    cout << "Enter Password" << endl;
-    string password;
-    string hash;
+    char *buf = (char *)malloc(maxLen + 1);
 
-    getline(cin, password);
-    cout << password << '\t' << sha256(password) << '\n';
+    for (int i = 1; i <= maxLen; ++i)
+    {
+        memset(buf, 0, maxLen + 1);
+        bruteImpl(buf, 0, i, password);//, alphabet);
+    }
 
-    // Berechnen:
-    double estimatedTime;
+    free(buf);
+}
 
-    // char startASCII = 33; // '!'
-    // char endASCII = 126; // '~'
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s password\n", argv[0]);
+    }
+    string password = argv[1];
+    int length = password.length();
 
-    char startASCII = 'a';
-    char endASCII = 'c';
+    char alphabet2 = *alphabet;
+    int alphabetSize = sizeof(alphabet)/sizeof(char);
+    cout << alphabetSize;
+    /*
 
-    // Bruteforce durchführen und Zeit messen: wir zählen die Clock Cycles und teilen durch Cycles / s
-    clock_t beginClock = clock();
-    clock_t endClock;
+    cout << "length:" <<length << endl;
 
-    int maxLength = 4;
+    beginClock = clock();
 
-    string foundPassword = bruteForce(hash, maxLength, startASCII, endASCII);
-    cout << "Entered password:" << '\t' << "Found password: " << foundPassword << '\t' << "Hash: " << hash;
-
-    endClock = clock();
+    bruteSequential(length, password);//, &alphabet_num);
+    cout << beginClock << endl;
+    cout << endClock << endl;
+    cout << CLOCKS_PER_SEC << endl;
     cout << (double)(endClock - beginClock) / CLOCKS_PER_SEC << endl;
+*/
+    /*
+
+        // Einlesen
+        cout << "Enter Password" << endl;
+        string password;
+        string hash;
+
+        getline(cin, password);
+        cout << password << '\t' << sha256(password) << '\n';
+
+        // Berechnen:
+        double estimatedTime;
+
+        // char startASCII = 33; // '!'
+        // char endASCII = 126; // '~'
+
+        char startASCII = 'a';
+        char endASCII = 'c';
+
+        // Bruteforce durchführen und Zeit messen: wir zählen die Clock Cycles und teilen durch Cycles / s
+        clock_t beginClock = clock();
+        clock_t endClock;
+
+        int maxLength = 4;
+
+        string foundPassword = bruteForce(hash, maxLength, startASCII, endASCII);
+        cout << "Entered password:" << '\t' << "Found password: " << foundPassword << '\t' << "Hash: " << hash;
+
+        endClock = clock();
+        cout << (double)(endClock - beginClock) / CLOCKS_PER_SEC << endl;
+        */
 
     return 0;
 }
